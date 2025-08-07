2af477f542a3106d4198696cf066408596147490
Suraj Mandal <113721983+Suraj4518@users.noreply.github.com>
2025-08-07 08:49:01 +0530

    Update serializer.py

--- restaurant_management/serializer.py
+++ restaurant_management/serializer.py
@@ -1,1 +1,7 @@
 
+from rest_framework import serializers
+
+class MenuItemSerializer(serializers.Serializer):
+    name = serializers.CharField()
+    description = serializers.CharField()
+    price = serializers.DecimalField(max_digits=6, decimal_places=2)
