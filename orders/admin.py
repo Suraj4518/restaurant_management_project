1f6ca4b924391ce2bacfaf7951ba7e482c0a1ced
Suraj Mandal <113721983+Suraj4518@users.noreply.github.com>
2025-08-22 20:51:34 +0530

    17527

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order
 
+class OrderAdmin(admin.ModeAdmin):
+    list_display = ['id','customer','total_amount','status','created_at']
+    list_filter = ['status','created_at']
+    search_fields = ['customer_username']
+
+admin.site.register(Order, OrderAdmin)
 # Register your models here.
